3610e082bae96a8f8a48e0ed79555f8b8775d216
DemonLord- <1441529822@qq.com>
2022-05-09 00:31:55 +0800

    增加了一个外部扩展，用于显示按钮 尝试使用BoxMuller算法生成动态分布的随机数，不过暂时未成功

--- Assets/Script/Meteorite-Related/MeteoriteCreateSystem.cs
+++ Assets/Script/Meteorite-Related/MeteoriteCreateSystem.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+public enum player //全局枚举 用来判断为那个玩家生成的内容
+{ 
+    pl=0,
+    p2=1
+}
 public class MeteoriteCreateSystem : Singleton<MeteoriteCreateSystem>
 {
     [Header("陨石的物体")]
     public GameObject Meteorite;//陨石
+    [Header("陨石父对象")]
+    public GameObject MeteortieFather;
+
     public enum creatingMode//生成的模式
     { 
         empty,//默认无模式
@@ -36,37 +43,100 @@ public class MeteoriteCreateSystem : Singleton<MeteoriteCreateSystem>
         {
             f.drawArea();
         }
+
     }
+
+    [EditorButton]
+    public GameObject CreateMeteorite(player _player=player.pl)
+    {
+        Rectangle AreaNow=Area[0];
+        switch (_player)
+        {
+            case player.pl:AreaNow = Area[0];break;
+            case player.p2:AreaNow = Area[1]; break;
+        }
+        float getX()
+        {
+            return (AreaNow.vector1.x - AreaNow.vector2.x) / 2 - AreaNow.vector1.x;
+        }
+        GameObject met;
+        Transform tr;
+        if (MeteortieFather == null) //如果父对象没有 则以系统本身为父对象
+        {
+            tr = transform;
+        }
+        else {
+            tr = MeteortieFather.transform;
+        }
+        met = Instantiate(Meteorite,tr);
+        var t = RandomPosGenerateForY(AreaNow);
+        met.transform.position=new Vector3(getX(),t,0);
+        //Debug.Log(t);
+        return met;
+    }
+    /// <summary>
+    /// 以下为生成陨石位置的随机生成代码，需要优化请在此修改
+    /// </summary>
+    /// <returns></returns>
+    float RandomPosGenerateForY(Rectangle _Area)//因为目前本项目与X位置无关，暂时只随机Y轴
+    {
+        
+        float BoxMuller()//正太分布函数 BoxMuller转化法
+        {
+            float p1, p2,w=0.0f,c;
+            do {
+                do
+                {
+                    p1 = Random.Range(0.01f, 1.01f);
+                    p2 = Random.Range(0.01f, 1.01f);
+                    w = p1 * p1 + p2 * p2;
+                } while (w == 0.0f || w >= 1.0f);
+                c = Mathf.Sqrt(-2 * Mathf.Log(p1)) * Mathf.Cos(2 * Mathf.PI * p2);
+            } while (c < -5 || c > 5);
+            c = c / 5 + 0.5f;
+            Debug.Log(c);
+            return c;
+        }
+        float Position;
+        Position = Mathf.Max(_Area.vector1.y, _Area.vector2.y) - BoxMuller() * Mathf.Abs(_Area.vector1.y - _Area.vector2.y);
+        return Position;
+
+
+    }
+    
 }
+
+
+
 [System.Serializable]
 public class Rectangle //矩形类 可以不用 但是最近C++学了 还是用一下
 {
-    public Vector2 p1, p2;
+    public Vector2 vector1, vector2;
     Rectangle(float x1,float y1,float x2,float y2)
     {
-        p1 = new Vector2(x1, y1);
-        p2 = new Vector2(x2, y2);
-    }
+        vector1 = new Vector2(x1, y1);
+        vector2 = new Vector2(x2, y2);
+    } 
     Rectangle(Vector2 v1,Vector2 v2)
     {
-        p1 = v1;
-        p2 = v2;
+        vector1 = v1;
+        vector2 = v2;
     }
     public void drawArea()
     {
         Vector3 tmp1,tmp2;
         Color c=Color.red;
-        tmp1 = new Vector3(p1.x, p1.y, 0);
-        tmp2 = new Vector3(p1.x, p2.y, 0);
+        tmp1 = new Vector3(vector1.x, vector1.y, 0);
+        tmp2 = new Vector3(vector1.x, vector2.y, 0);
         Debug.DrawLine(tmp1,tmp2,c);
         tmp1 = tmp2;
-        tmp2.Set(p2.x, p2.y, 0);
+        tmp2.Set(vector2.x, vector2.y, 0);
         Debug.DrawLine(tmp1, tmp2, c);
         tmp1 = tmp2;
-        tmp2.Set(p2.x, p1.y, 0);
+        tmp2.Set(vector2.x, vector1.y, 0);
         Debug.DrawLine(tmp1, tmp2, c);
         tmp1 = tmp2;
-        tmp2.Set(p1.x, p1.y, 0);
+        tmp2.Set(vector1.x, vector1.y, 0);
         Debug.DrawLine(tmp1, tmp2, c);
     }
 }
