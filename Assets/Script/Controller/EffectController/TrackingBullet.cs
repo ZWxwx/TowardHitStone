eec56172775090a2f2f525db37a0318d16dada2d
DemonLord- <1441529822@qq.com>
2022-05-19 19:57:36 +0800

    完成了追踪子弹功能 以防万一 本次没有提交场景文件 只提交了TackingBullet文件

--- Assets/Script/Controller/EffectController/TrackingBullet.cs
+++ Assets/Script/Controller/EffectController/TrackingBullet.cs
@@ -17,38 +17,73 @@ public class TrackingBullet : MonoBehaviour
         bullet = GetComponent<BulletController>();
         rb = GetComponent<Rigidbody2D>();
         //ReTracking();
+        spinSpeed = 200f;
         currentTarget = GameObject.Find("MeteoriteTest").GetComponent<MeteoriteObject>();
+        InvokeRepeating("TrackMove", 0.1f, 0.01f);
+        targetDir = rb.velocity;
     }
 
 
     // Update is called once per frame
     void Update() {
 
-        Vector3 x=transform.position;//子弹位置
-        Vector3 y=currentTarget.transform.position;//目标位置
-        Vector3 z= transform.rotation.eulerAngles;//子弹目前方向
-        float angleB= Mathf.Atan2(x.y - y.y, x.x - y.x)*180/Mathf.PI+360;
-        float angleA = z.z;
-		if (a >= 10)
-		{
-            a = 0;
-            rb.velocity = -new Vector2(Mathf.Sin(angleA), Mathf.Cos(angleA)) * bullet.speed; 
-        }
-        if ((Mathf.Abs(angleA - angleB) < 180 && angleA < angleB)|| (Mathf.Abs(angleA - angleB) > 180 && angleA > angleB))
-		{
-            transform.Rotate(new Vector3(0, 0, 10));
-            Debug.Log("左拐");
-        }
-		else
-		{
-            transform.Rotate(new Vector3(0, 0, -10));
-            Debug.Log("右拐");
+        //      Vector3 x=transform.position;//子弹位置
+        //      Vector3 y=currentTarget.transform.position;//目标位置
+        //      Vector3 z= transform.rotation.eulerAngles;//子弹目前方向
+        //      float angleB= Mathf.Atan2(x.y - y.y, x.x - y.x)*180/Mathf.PI+360;
+        //      float angleA = z.z;
+        //if (a >= 10)
+        //{
+        //          a = 0;
+        //          rb.velocity = -new Vector2(Mathf.Sin(angleA), Mathf.Cos(angleA)) * bullet.speed; 
+        //      }
+        //      if ((Mathf.Abs(angleA - angleB) < 180 && angleA < angleB)|| (Mathf.Abs(angleA - angleB) > 180 && angleA > angleB))
+        //{
+        //          transform.Rotate(new Vector3(0, 0, 10));
+        //          Debug.Log("左拐");
+        //      }
+        //else
+        //{
+        //          transform.Rotate(new Vector3(0, 0, -10));
+        //          Debug.Log("右拐");
 
-        }
-        Debug.LogFormat("子弹位置:{0}   目标位置:{1}    子弹目前方向向量:{2}    目标方向:{3}  目前方向:{4}", x, y, z, angleB, angleA);
-        a++;
+        //      }
+        //      Debug.LogFormat("子弹位置:{0}   目标位置:{1}    子弹目前方向向量:{2}    目标方向:{3}  目前方向:{4}", x, y, z, angleB, angleA);
+        //      a++;
+        //TrackMove();
+       transform.eulerAngles+=LookAt2D(transform,transform.position+targetDir.normalized);
     }
+    public Vector3 LookAt2D(Transform from, Vector3 to)
+    {
+        float dx = to.x - from.transform.position.x;
+        float dy = to.y - from.transform.position.y;
+        float rotationZ = Mathf.Atan2(dy, dx) * 180 / Mathf.PI;
+       
 
+        float originRotationZ = from.eulerAngles.z;
+        float addRotationZ = rotationZ - originRotationZ;
+        if (addRotationZ > 180)
+            addRotationZ -= 360;
+        
+        return new Vector3(0, 0, addRotationZ);
+    } //抄来的
+
+    Vector3 targetDir;
+    public float spinSpeed;
+    void TrackMove()
+    {
+        Vector3 speedDir = rb.velocity.normalized;
+        Vector3 DisVec = currentTarget.transform.position - transform.position;
+        speedDir = new Vector3(speedDir.x * spinSpeed, speedDir.y * spinSpeed, 0);
+        //DisVec = new Vector3(DisVec.x * test2, DisVec.y * test2, 0);
+        targetDir = (speedDir + DisVec).normalized;
+        rb.velocity = targetDir * bullet.speed;
+       // Debug.Log(spinSpeed);
+        if (spinSpeed > 15f)
+        {
+            spinSpeed -= 2f;
+        }
+    }
 	void ReTracking()
 	{
 		float minDistance = 32565;
